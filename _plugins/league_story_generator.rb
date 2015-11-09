f6c25915e0a76f7277cea7943f04a61fc82efe53
léo galley <contact@kirkas.ch>
2015-11-09 10:32:21 -0800

    add league-story as template, not layout

--- _plugins/league_story_generator.rb
+++ _plugins/league_story_generator.rb
@@ -8,7 +8,7 @@ module Jekyll
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'league-story.html')
+      self.read_yaml(File.join(base, '_templates'), 'league-story.md')
 
       self.data['league_story'] = league_story
       self.data['title'] = league_story['title']
